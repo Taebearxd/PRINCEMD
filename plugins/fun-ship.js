621a1ecadcab4c07a6ef68712a114b7cb188a354
Taebearxd <149920040+Taebearxd@users.noreply.github.com>
2024-12-15 09:36:03 +0530

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -5,7 +5,7 @@ function handler(m, { groupMetadata }) {
     let b
     do b = ps.getRandom()
     while (b === a)
-    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations 💖🍻`, null, {
+    m.reply(`${toM(a)} ❤️ ${toM(b)}\n𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀‼️ 𝗡𝗼𝘄 𝗺𝗮𝗸𝗲 𝗸𝗶𝗱𝘀 🍑💦`, null, {
         mentions: [a, b]
     })
 }
